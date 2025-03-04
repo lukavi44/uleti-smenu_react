80f74ffc696eb5c3144a3a950e64b53b44a113ba
Luka Ladisic <l.ladisic@codenovasolutions.net>
2025-03-04 08:55:02 +0100

    Initial commit

---
+++ uleti-smenu/src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css'
+import Footer from './components/Footer/Footer'
+import Header from './components/Header/Header'
+import HomePage from './pages/Home/Home'
+import JobPosts from './pages/JobPosts/JobPosts'
+
+function App() {
+
+  return (
+    <>
+     <Header/>
+     <HomePage/>
+     <JobPosts/>
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, at laboriosam expedita est labore excepturi eligendi atque rerum velit saepe voluptatibus aliquam officiis quam, commodi, provident tempore. Quae, laboriosam iste obcaecati a dolorum quis neque, iure enim reiciendis pariatur eligendi est nemo reprehenderit harum sint earum impedit ratione illum amet praesentium? Accusantium, illum. Facilis ut magnam consequuntur alias magni dignissimos, quisquam earum asperiores dolore sequi velit officia debitis veritatis tempora nemo vel reiciendis minus ipsa odio recusandae. Alias consequatur suscipit dignissimos odit culpa labore autem? Quidem earum architecto error explicabo temporibus numquam vitae veritatis nihil perspiciatis possimus cupiditate aliquam ut consequuntur blanditiis sunt, perferendis assumenda molestiae? Sunt, sed! Adipisci laborum consequatur amet sapiente. Enim nam voluptates consectetur ex ipsam quasi, reprehenderit, laudantium officia corrupti atque, incidunt natus modi dolorem est corporis quo fugiat animi excepturi? Voluptas distinctio eius, maiores, hic recusandae, natus ad iusto fugiat enim ducimus porro quasi nihil officiis deserunt soluta ut voluptate consectetur! Quisquam, incidunt facilis? Distinctio nostrum eligendi error eos ipsa corporis velit consequatur voluptatum libero modi voluptates saepe commodi, eum fugiat cupiditate illum qui natus! Suscipit consequatur cum iusto ratione similique rerum, minus porro amet iure, cumque animi ipsam sapiente sit aspernatur ad eveniet architecto!</p>
+     <Footer/>
+    </>
+  )
+}
+
+export default App
