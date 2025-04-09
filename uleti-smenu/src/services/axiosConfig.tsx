4d680c1b67e529704fb7d3719ab103b17a793d09
Luka Ladisic <l.ladisic@codenovasolutions.net>
2025-04-09 10:15:58 +0200

    axios configuration

---
+++ uleti-smenu/src/services/axiosConfig.tsx
@@ -0,0 +1,70 @@
+import axios from "axios";
+import { toast } from "react-toastify";
+import getApiBaseUrl from "../configuration/config";
+
+const apiBaseURL = getApiBaseUrl();
+
+const axiosInstance = axios.create({
+  withCredentials: true,
+  baseURL: apiBaseURL,
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+});
+
+axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("AccessToken");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const errorMessage = error.response?.data as unknown as string;
+    const originalRequest = error.config;
+
+    const handleSessionExpired = () => {
+      localStorage.removeItem("AccessToken");
+      localStorage.removeItem("RefreshToken");
+      toast.error("Session expired, please login again.");
+      window.location.href = "/login";
+    };
+
+    if (error.response.status === 401 && !originalRequest._retry && errorMessage !== "Invalid refresh token.") {
+      originalRequest._retry = true;
+      try {
+        const _accessToken = localStorage.getItem("AccessToken");
+        const _refreshToken = localStorage.getItem("RefreshToken");
+
+        if (_accessToken && _refreshToken) {
+          const body = {
+            accessToken: _accessToken,
+            refreshToken: _refreshToken
+          };
+        //   const { data } = await RefreshToken(body);
+
+          localStorage.setItem('AccessToken', body.accessToken);
+          localStorage.setItem('RefreshToken', body.refreshToken)
+
+          originalRequest.headers['Authorization'] = `Bearer ${body.accessToken}`;
+
+          return axiosInstance(originalRequest);
+        }
+      } catch (err) {
+        return Promise.reject(err)
+      }
+    } else if (error.response.status === 401 && errorMessage === "Invalid refresh token.") {
+      handleSessionExpired();
+    } else if (error.response.status === 500) {
+      toast.error("Unexpected server error");
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
