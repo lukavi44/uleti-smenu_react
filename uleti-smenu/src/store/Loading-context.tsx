62eae079fba9c0cf21d223f5605827b75e4b1156
Luka Ladisic <l.ladisic@codenovasolutions.net>
2025-04-12 17:50:25 +0200

    loader

---
+++ uleti-smenu/src/store/Loading-context.tsx
@@ -0,0 +1,23 @@
+import { createContext, useState, ReactNode, useContext } from "react";
+
+interface LoadingContextType {
+  isLoading: boolean;
+  setLoading: (loading: boolean) => void;
+}
+
+const LoadingContext = createContext<LoadingContextType>({
+    isLoading: false,
+    setLoading: () => {}
+});
+
+export const LoadingProvider = ({ children }: { children: ReactNode }) => {
+  const [isLoading, setLoading] = useState(false);
+
+  return (
+    <LoadingContext.Provider value={{ isLoading, setLoading }}>
+      {children}
+    </LoadingContext.Provider>
+  );
+};
+
+export default LoadingContext;
